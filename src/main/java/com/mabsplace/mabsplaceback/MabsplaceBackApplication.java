280eb1ef899e65f242fcbbd584d3a0474c51c1db
Karlmabs <maboukarl2@gmail.com>
2023-12-11 17:35:39 +0100

    Initialize Spring Boot project with Maven wrapper and .gitignore
    This commit initializes a Spring Boot project and adds Maven wrapper files to support consistent build environments. It also introduces a .gitignore file to ensure that unnecessary build and IDE artifacts are not versioned. The essential project structure and application files have been generated, setting the foundation for further development.

---
+++ src/main/java/com/mabsplace/mabsplaceback/MabsplaceBackApplication.java
@@ -0,0 +1,13 @@
+package com.mabsplace.mabsplaceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabsplaceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabsplaceBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mabsplace/mabsplaceback/MabsplaceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabsplace.mabsplaceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabsplaceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
