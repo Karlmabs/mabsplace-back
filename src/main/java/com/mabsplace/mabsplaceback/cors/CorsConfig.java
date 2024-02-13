5fa604cbf1ecdf7d8934b24214cb688792f8b7d2
Karlmabs <maboukarl2@gmail.com>
2024-02-13 22:09:59 +0100

    Added patch method to allowed methods

--- src/main/java/com/mabsplace/mabsplaceback/cors/CorsConfig.java
+++ src/main/java/com/mabsplace/mabsplaceback/cors/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
             .allowedHeaders("*")
             .maxAge(3600);
   }
