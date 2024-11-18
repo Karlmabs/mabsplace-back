0164497cb9113ab4651aa1a3f417d4a376cd710f
karlmabs <maboukarl2@gmail.com>
2024-11-18 14:36:17 +0100

    added logging on create payment

--- src/main/java/com/mabsplace/mabsplaceback/domain/services/PaymentService.java
+++ src/main/java/com/mabsplace/mabsplaceback/domain/services/PaymentService.java
@@ -1,5 +1,6 @@
 package com.mabsplace.mabsplaceback.domain.services;
 
+import com.mabsplace.mabsplaceback.domain.controllers.TransactionController;
 import com.mabsplace.mabsplaceback.domain.dtos.payment.PaymentRequestDto;
 import com.mabsplace.mabsplaceback.domain.dtos.subscription.SubscriptionRequestDto;
 import com.mabsplace.mabsplaceback.domain.entities.Payment;
@@ -9,6 +10,8 @@ import com.mabsplace.mabsplaceback.domain.enums.SubscriptionStatus;
 import com.mabsplace.mabsplaceback.domain.mappers.PaymentMapper;
 import com.mabsplace.mabsplaceback.domain.repositories.*;
 import com.mabsplace.mabsplaceback.exceptions.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -33,6 +36,8 @@ public class PaymentService {
 
     private final DiscountService discountService;
 
+    private static final Logger logger = LoggerFactory.getLogger(PaymentService.class);
+
     public PaymentService(PaymentRepository paymentRepository, PaymentMapper paymentMapper, UserRepository userRepository, CurrencyRepository currencyRepository, MyServiceRepository myServiceRepository, SubscriptionPlanRepository subscriptionPlanRepository, WalletService walletService, SubscriptionService subscriptionService, DiscountService discountService) {
         this.paymentRepository = paymentRepository;
         this.paymentMapper = paymentMapper;
@@ -46,16 +51,26 @@ public class PaymentService {
     }
 
     public Payment createPayment(PaymentRequestDto paymentRequestDto) throws RuntimeException {
+
+        logger.info("Creating payment for user with id: " + paymentRequestDto.getUserId());
+
         Payment entity = paymentMapper.toEntity(paymentRequestDto);
         User user = userRepository.findById(paymentRequestDto.getUserId()).orElseThrow(() -> new ResourceNotFoundException("User", "id", paymentRequestDto.getUserId()));
+
+        logger.info("User found: " + user.toString());
 
         double discount = discountService.getDiscountForUser(user.getId());
         BigDecimal amountAfterDiscount = paymentRequestDto.getAmount().subtract(BigDecimal.valueOf(discount));
 
+        logger.info("Amount after discount: " + amountAfterDiscount);
+
         boolean checkBalance = walletService.checkBalance(user.getWallet().getId(), amountAfterDiscount);
+
+        logger.info("Checking balance: " + checkBalance);
 
         if (!checkBalance) throw new RuntimeException("Insufficient funds");
 
+        logger.info("Sufficient funds");
 
         entity.setUser(user);
         entity.setCurrency(currencyRepository.findById(paymentRequestDto.getCurrencyId()).orElseThrow(() -> new ResourceNotFoundException("Currency", "id", paymentRequestDto.getCurrencyId())));
@@ -64,10 +79,13 @@ public class PaymentService {
         entity.setAmount(amountAfterDiscount);
         entity.setStatus(PaymentStatus.PAID);
 
+        logger.info("Debiting wallet");
 
         walletService.debit(user.getWallet().getId(), amountAfterDiscount);
 
         Payment savedPayment = paymentRepository.save(entity);
+
+        logger.info("Payment saved");
 
         // Create a subscription without a profile or an account associated to it
         SubscriptionRequestDto subscriptionDto = SubscriptionRequestDto.builder()
@@ -79,7 +97,11 @@ public class PaymentService {
                 .status(SubscriptionStatus.INACTIVE) // Subscription is inactive until payment is confirmed
                 .build();
 
+        logger.info("Creating subscription");
+
         subscriptionService.createSubscription(subscriptionDto);
+
+        logger.info("Subscription created");
 
         return savedPayment;
     }
