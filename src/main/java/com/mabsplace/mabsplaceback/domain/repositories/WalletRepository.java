8e2f08f8d6ac73adc65489860a2663d9c01286f2
karlmabs <maboukarl2@gmail.com>
2024-11-18 15:04:47 +0100

    updated the find wallet query

--- src/main/java/com/mabsplace/mabsplaceback/domain/repositories/WalletRepository.java
+++ src/main/java/com/mabsplace/mabsplaceback/domain/repositories/WalletRepository.java
@@ -2,10 +2,15 @@ package com.mabsplace.mabsplaceback.domain.repositories;
 
 import com.mabsplace.mabsplaceback.domain.entities.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
-  Wallet findByUserId(Long userId);
+
+    //get wallet by user id with query
+    @Query("SELECT w FROM Wallet w WHERE w.user.id = :userId")
+    Wallet findByUserId(@Param("userId") Long userId);
 
 }
