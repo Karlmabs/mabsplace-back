c60efe06520db1e7679a58d0d8668ea58b95b353
karlmabs <maboukarl2@gmail.com>
2025-11-24 03:49:38 +0100

    Remove JavaMailSender configuration from MabsplaceBackApplication to streamline email handling

--- src/main/java/com/mabsplace/mabsplaceback/MabsplaceBackApplication.java
+++ src/main/java/com/mabsplace/mabsplaceback/MabsplaceBackApplication.java
@@ -5,13 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Properties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
@@ -24,23 +20,7 @@ public class MabsplaceBackApplication {
         return new RestTemplate();
     }
 
-    @Bean
-    public JavaMailSender mailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
 
-        mailSender.setUsername("mabsplace2024@gmail.com");
-        mailSender.setPassword("hofq zhhw anvf bzro");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-//    props.put("mail.debug", "true");
-
-        return mailSender;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MabsplaceBackApplication.class, args);
